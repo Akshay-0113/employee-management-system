d4a84883d1d61e939af184767fd2ad1fa83baf42
Akshay Mondal <83979383+Akshay-0113@users.noreply.github.com>
2024-07-15 12:10:58 +0530

    Add files via upload

--- src/components/view-employee.component.js
+++ src/components/view-employee.component.js
@@ -1,72 +1,83 @@
-import React,
-{
-	useState,
-	useEffect
-} from "react";
-import {useParams, useNavigate} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import EmpDisp
-	from "./EmpDisp";
+import { Table } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
-// due to lack of time i duplicated the add employee form and made certain changes to give view employee page. it can be improved
+// Function to format date to dd-mm-yyyy
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+};
 
-const ViewEmployee = (props) => {
-	const {empId} = useParams();
-	const history = useNavigate();
-	const [formValues, setFormValues] =
-		useState(
-			{	
-				name: "",
-				email: "",
-				location: '',
-                phone: '',
-                department: '',
-                dob: '',
-                gender: '',
-                salary: '',
-                employmentType: ''
-			});
+const ViewEmployee = () => {
+    const [employee, setEmployee] = useState(null);
+    const { empId } = useParams();
 
-	useEffect(() => {
-		axios.get("http://localhost:5000/view-employee/"+ empId)
-			.then((res) => {
-				const {
-					name,
-					email,
-					location,
-                    phone,
-                    department,
-                    dob,
-                    gender,
-                    salary,
-                    employmentType
-				} = res.data;
-				setFormValues(
-					{
-						name,
-						email,
-						location,
-                        phone,
-                        department,
-                        dob,
-                        gender,
-                        salary,
-                        employmentType
-					});
-			})
-			.catch(
-				(err) =>
-					console.log(err)
-			);
-	});
+    useEffect(() => {
+        axios.get(`http://localhost:5000/view-employee/`+empId)
+            .then((res) => {
+                setEmployee(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [empId]);
 
-	return (
-		<EmpDisp
-			initialValues={formValues}
-			// onSubmit={onSubmit}
-			enableReinitialize>
-		</EmpDisp>
-	);
+    if (!employee) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="table-wrapper2">
+            <Table striped bordered hover>
+                <tbody>
+                    <tr>
+                        <td>Employee ID:</td>
+                        <td>{employee.empId}</td>
+                    </tr>
+                    <tr>
+                        <td>Name:</td>
+                        <td>{employee.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>{employee.email}</td>
+                    </tr>
+                    <tr>
+                        <td>Location:</td>
+                        <td>{employee.location}</td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>{employee.phone}</td>
+                    </tr>
+                    <tr>
+                        <td>Department:</td>
+                        <td>{employee.department}</td>
+                    </tr>
+                    <tr>
+                        <td>Date of Birth:</td>
+                        <td>{formatDate(employee.dob)}</td>
+                    </tr>
+                    <tr>
+                        <td>Gender:</td>
+                        <td>{employee.gender}</td>
+                    </tr>
+                    <tr>
+                        <td>Salary:</td>
+                        <td>{employee.salary}</td>
+                    </tr>
+                    <tr>
+                        <td>Employment Type:</td>
+                        <td>{employee.employmentType}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 };
 
 export default ViewEmployee;
